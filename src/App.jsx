0a354004f83b02867c5227a16968f71a71fc451d
dhruv <dhruvmehalawat34@gmail.com>
2026-07-10 18:39:59 +0530

    Product section first half done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 import Product from "./components/Product";
+import {ScrollTrigger, SplitText} from "gsap/all";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
