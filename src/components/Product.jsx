1f10bc04ee85feed666d40bbb851d1090f6ca781
dhruv <dhruvmehalawat34@gmail.com>
2026-07-14 13:51:14 +0530

    showcase section complete

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -39,7 +39,7 @@ const Product = () => {
 
             
         </div>
-        <Canvas id="canvas">
+        <Canvas id="canvas" camera={{position :[0,2,5], fov: 50, near: 0.1, far: 100}}>
             <StudioLights/>
             
             <ModelSwitcher scale={ isMobile? scale - 0.03 : scale} isMobile={isMobile}/>            
